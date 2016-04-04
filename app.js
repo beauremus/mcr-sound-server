4fdeb4e60bac85fa7461c521d5795d441c7f03d4
Beau Harrison <beau@fnal.gov>
2016-04-04 17:11:47 +0000

    Install and implement IP filters

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var ipfilter = require('express-ipfilter');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
@@ -22,8 +23,19 @@ app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
+app.use('/test', routes);
 app.use('/voiceTest', voiceTest);
+
+// IP allow list; all others denied
+var ips = ['131.225.121.20', // cns1pc.fnal.gov
+            ['131.225.121.22','131.225.121.31'], // cns2-11
+            ['131.225.121.33','131.225.121.35'], // cns12-14
+            '131.225.122.10', // adops126896.fnal.gov
+            '131.225.122.76', // newhart
+            '131.225.122.90']; // djohnson
+
+// ipFiltering
+app.use(ipfilter(ips, {mode: 'allow'}));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

f539d129758d890ddefb63f9562c38671c1ca1e7
Beau Harrison <beauharrison@gmail.com>
2016-05-25 23:48:51 -0500

    MCROPS running version

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ module.exports = function(io) {
     var router = app.Router();
     var tcpsock = require("net");
 
-    var tcp_HOST = process.env.IP || '131.225.122.10';
+    var tcp_HOST = process.env.IP || '131.225.121.53';
     var tcp_PORT = (tcp_HOST == process.env.IP) ? 8081 : 1661;
 
     var queue = [];
@@ -12,12 +12,13 @@ module.exports = function(io) {
         res.render('index', { title: 'MCR Sound Server' });
     });
 
+    // TCP connections from PA1661 currently on cns10
     var server = tcpsock.createServer(function(sock) {
         console.log('TCP CONNECTION: ' + sock.remoteAddress +':'+ sock.remotePort);
         io.emit("httpServer", "Sound server connected");
 
         sock.on('data', function(data) {
-            console.log('DATA: ' + data);
+            console.log('ALARMS DATA: ' + data);
             queue.push(data);
             io.emit("httpServer", alarmsTCP2Speech(queue[0]));
         });
@@ -40,6 +41,36 @@ module.exports = function(io) {
             console.log('ERROR : ' + err);
         });
     }).listen(tcp_PORT, tcp_HOST);
+
+    // TCP connections from PA4045 currently on cns4
+    tcpsock.createServer(function(sock) {
+        console.log('TCP CONNECTION: ' + sock.remoteAddress +':'+ sock.remotePort);
+        io.emit("httpServer", "State server connected");
+
+        sock.on('data', function(data) {
+            console.log('STATE DATA: ' + data);
+            queue.push(data);
+            io.emit("httpServer", stateTCP2Speech(queue[0]));
+        });
+
+        sock.on('close', function(data) {
+            io.emit("httpServer", "Sound server connection removed");
+            console.log('CLOSED: ' + sock.remoteAddress +' '+ sock.remotePort);
+        });
+
+        sock.on('end', function(data) {
+            console.log('END DATA : ' + data);
+            console.log('A user disconnected');
+        });
+
+        io.on('disconnect', function() {
+            sock.end();
+        });
+
+        sock.on('error', function(err) {
+            console.log('ERROR : ' + err);
+        });
+    }).listen('3721', '131.225.121.53');
 
     io.on('connection', function(socket) {
         console.log('HTTP server listening to ' + tcp_HOST +':'+ tcp_PORT);
@@ -107,6 +138,40 @@ function alarmsTCP2Speech(data) {
             return "Switchyard";
         case (/T/).test(dataString):
             return "Tevatron";
+        default:
+            return "Unknown speech request";
+    }
+}
+
+function stateTCP2Speech(data) {
+    var dataString = data.toString('ascii');
+    switch (true) {
+        case (/3/).test(dataString):
+            return "P 2 Line Permit";
+        case (/4/).test(dataString):
+            return "P 1 Line Permit";
+        case (/5/).test(dataString):
+            return "B N B Permit";
+        case (/6/).test(dataString):
+            return "NuMI Permit";
+        case (/7/).test(dataString):
+            return "Recycler Permit";
+        case (/8/).test(dataString):
+            return "Switchyard Permit";
+        case (/9/).test(dataString):
+            return "Tevatron Permit";
+        case (/10/).test(dataString):
+            return "Main Injector Permit";
+        case (/11/).test(dataString):
+            return "P Bar Permit";
+        case (/12/).test(dataString):
+            return "Booster Permit";
+        case (/13/).test(dataString):
+            return "Linac Permit";
+        case (/14/).test(dataString):
+            return "Linac Downstream Permit";
+        case (/15/).test(dataString):
+            return "Linac Upstream Permit";
         default:
             return "Unknown speech request";
     }
